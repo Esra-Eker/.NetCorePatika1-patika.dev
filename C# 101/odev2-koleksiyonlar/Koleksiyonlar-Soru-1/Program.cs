aa5c15126fec4466bbc57e7ce45ee483c89ec216
esra-eker <elifesraeker@gmail.com>
2022-02-20 23:48:47 +0300

    ödev2

---
+++ C# 101/odev2-koleksiyonlar/Koleksiyonlar-Soru-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Koleksiyonlar_Soru_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList asal = new ArrayList();
+            ArrayList asaldegil = new ArrayList();
+            for(int i=0; i<20; i++)
+            {
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                for (int j=1; j<=sayi; j++)
+                {
+                    if(sayi%2 == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
