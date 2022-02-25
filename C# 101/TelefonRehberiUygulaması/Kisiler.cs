d7a267838880b127bb66a3d744b112d022e8674f
esra-eker <elifesraeker@gmail.com>
2022-02-25 23:30:30 +0300

    .

---
+++ C# 101/TelefonRehberiUygulaması/Kisiler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelefonRehberiUygulaması
+{
+    class Kisiler
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string No { get; set; }
+
+
+        public Kisiler(string ad, string soyad)
+        {
+            this.Ad = ad;
+            this.Soyad = soyad;
+        }
+
+    }
+}
---
+++ C# 101/TelefonRehberiUygulaması/Methodlar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelefonRehberiUygulaması
+{
+    public class Methodlar 
+    {
+        string[] isimler = new string[] { "Deniz", "Tekin", "Tugce" };
+        public void YeniNumaraKaydet(string isim)
+        {
+              isimler[3] = isim;
+              Console.WriteLine("İsim Başarıyla Kaydedildi...");
+            for (int i = 0; i <4; i++)
+            {
+                Console.WriteLine(isimler[i]);
+            }
+        }
+    }
+}
